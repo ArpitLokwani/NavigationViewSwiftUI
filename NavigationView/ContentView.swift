fd39ed41dc8c5a8f4e45b80c15a9cc51d3f67faf
ArpitLokwani <arpit lokwani>
2020-02-24 09:35:40 +0530

    Initial Commit

---
+++ NavigationView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationView
+//
+//  Created by Arpit Lokwani on 24/02/20.
+//  Copyright © 2020 Arpit Lokwani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
